5ad0a664259eaea13ce78b696f3ff7ae250b7862
premnath pichaimuthu <premwebdev+dev@gmail.com>
2023-07-06 11:38:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import ReactDOMServer from "react-dom/server";
 
-function App() {
+const App = () => {
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    // This code will be executed on the server.
+    const renderedHtml = ReactDOMServer.renderToString(<h1>Hello, {name}</h1>);
+    return () => {
+      // This code will be executed on the client.
+     // ReactDOM.hydrate(renderedHtml, document.getElementById("root"));
+    };
+  }, [name]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
     </div>
   );
-}
+};
 
 export default App;
